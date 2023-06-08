9265418dc0d7417b698c91852835c84ea07323eb
Student debil <130003078+Janek-Ratkun@users.noreply.github.com>
2023-06-08 11:04:15 +0200

    Create rime-marinator.py
    Program główny

---
+++ rime-marinator.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+class Rime_Marinator(object):
+	def __init__(self):	
+		self.okno = Tk()
+		self.okno.geometry("1024x600")
+		self.okno.title("Rime Marinator")
+		self.background_colour = '#121212'
+		self.okno["bg"] = self.background_colour
+		
+		self.etykieta1 = Label(self.okno, font=("Verdana",18,"bold"), foreground='white',background='#272727',text='Rime Marinator - Słownik rymów')
+		self.etykieta1.place(x=0,y=0,width=1024,height=64)
+		self.okno.mainloop()
+		
+def main():
+	rime_marinator = Rime_Marinator()
+main()
